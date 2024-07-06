fe16e00fe83010bedd2ce8dbf6b9d2c28efe240f
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-06 11:37:19 +0530

    Check for hand occurence

--- hand-tracking/handTrackingMin.py
+++ hand-tracking/handTrackingMin.py
@@ -8,6 +8,13 @@ hands = mpHands.Hands() #Create instance of Hands
 
 while True:
     success, img = cap.read()
+
+    #Convert to RGB bcz mediapipe expects the input image to be in RGB format
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    #If Hand occurs in a frame it will return values otherwise none
+    print(results.multi_hand_landmarks)
     cv.imshow("Video", img)
     if cv.waitKey(20) & 0xFF == ord('q'):
         break
