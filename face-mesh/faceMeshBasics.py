06abca1044876173d5bf65f717b10c0d21acd413
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-22 23:48:24 +0530

    Detect face and display mesh

---
+++ face-mesh/faceMeshBasics.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+cap = cv.VideoCapture(0)
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
+
+while True:
+    success, img = cap.read()
+    img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = faceMesh.process(img_rgb)
+
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_TESSELATION)
+            # mpDraw.draw_landmarks(img, faceLms) # If we don't want to display connections
+
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv.putText(img, f"FPS: {int(fps)}", (10, 35), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv.imshow("Video", img)
+
+    if cv.waitKey(20) & 0xFF == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
