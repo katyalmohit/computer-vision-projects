660e3a859c14816a673f32cdee45f041b451075d
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-25 11:51:23 +0530

    Modify  system volume

---
+++ hand-tracking/volumeHandControl.py
@@ -0,0 +1,81 @@
+import cv2 as cv
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+import alsaaudio
+from subprocess import call
+
+valid = False
+
+while not valid:
+    volume = input('What volume? > ')
+
+    try:
+        volume = int(volume)
+        minVol, maxVol = 0, 100
+        if (volume <= maxVol) and (volume >= minVol):
+            call(["amixer", "-D", "pulse", "sset", "Master", str(volume)+"%"])
+            valid = True
+
+    except ValueError:
+        pass
+
+
+#######################
+# Width and height of the feed for webcam
+wCam, hCam = 640, 480
+#######################
+
+cap = cv.VideoCapture(0)
+cap.set(3, wCam) #prop ID at No. 3 is width
+cap.set(4, hCam) #prop ID at No. 4 is height
+
+detector = htm.HandDetector() # We can also change the parameters from here
+
+pTime = 0
+
+
+while True:
+    success, img = cap.read()
+
+    detector.findHands(img, draw=True)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+
+        # print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2)//2, (y1 + y2)//2
+
+        cv.circle(img, (x1, y1), 8, (255, 0, 255), -1)
+        cv.circle(img, (x2, y2), 8, (255, 0, 255), -1)
+
+        cv.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+        cv.circle(img, (cx, cy), 8, (255, 0, 255), -1)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        print(length)
+
+        # Hand range 10 - 250
+        # Volume range 0 - 100
+
+
+        if length<50:
+            cv.circle(img, (cx, cy), 8, (0, 0, 255), -1)
+
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv.putText(img, f"FPS: {int(fps)}", (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+    cv.imshow("Video", img)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
