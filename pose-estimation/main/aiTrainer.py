9e561d721d4ca20bf0e41508947cd3c17239d2e9
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-28 01:27:01 +0530

    Display sidebar

--- pose-estimation/main/aiTrainer.py
+++ pose-estimation/main/aiTrainer.py
@@ -4,7 +4,8 @@ import numpy as np
 import PoseModule as pm
 
 
-cap = cv.VideoCapture('../videos/dumbbells.mp4')
+# cap = cv.VideoCapture('../videos/dumbbells.mp4')
+cap = cv.VideoCapture(0)
 
 count = 0
 dir = 0         # 0 denotes going up, 1 denotes going down
@@ -25,20 +26,32 @@ while True:
         # Left Arm
         angle = detector.findAngle(img, 11, 13, 15)
         per = np.interp(angle, (30, 130), (0, 100))
+        bar = np.interp(angle, (30, 130), (100, 400)) # Mapping the values of angle to bar values
+
         # print(angle, per)
 
         # Check for the dumbbell curls
+        color = (255, 0, 255)
         if per == 100:
+            color = (0, 255, 0)
             if dir==1:
                 count+=0.5
                 dir = 0
+                
         if per == 0:
+            color = (0, 255, 0)
             if dir==0:
                 count+=0.5
                 dir = 1
         # print(count)
+
+        # Draw bar
+        cv.rectangle(img, (800, 100), (850, 400), color, 3)
+        cv.rectangle(img, (800, int(bar)), (850, 400), color, -1)
+        cv.putText(img, f'{str(100 - int(per))}%', (760, 70), cv.FONT_HERSHEY_PLAIN, 3, color, 3)
+
         cv.rectangle(img, (0, 450), (250, 500), (0, 255, 0), -1)
-        cv.putText(img, f'Count: {str(int(count))}', (10, 490), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 5)
+        cv.putText(img, f'Count: {str(int(count))}', (10, 490), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 5)
 
         ## If we want to display the count in decimals
         # cv.putText(img, str(count), (50, 100), cv.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
