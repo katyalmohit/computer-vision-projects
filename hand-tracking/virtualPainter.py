3769fa892dcf1bf8ed70a81d376972328b5a4c30
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-30 22:59:48 +0530

    Set header image

--- hand-tracking/virtualPainter.py
+++ hand-tracking/virtualPainter.py
@@ -4,6 +4,14 @@ import time
 import HandTrackingModule as htm
 import os
 
+def rescaleImage(image, height, width):
+    width = int(width)
+    height = int(height)
+    
+    dimensions = (width, height) #tuple containing rescaled width and height of image
+    
+    return cv.resize(image, dimensions, interpolation = cv.INTER_AREA)
+
 
 folderPath = "./resources/header-images"
 
@@ -19,6 +27,7 @@ for imPath in myList:
 # print(len(overlayList))
 
 header = overlayList[0]
+header = rescaleImage(header, 125, 1280)
 
 cap = cv.VideoCapture(0)
 cap.set(3, 1280)
@@ -26,6 +35,10 @@ cap.set(4, 720)
 
 while True:
     success, img = cap.read()
+
+    # Setting the header image
+    img[0:125, 0:1280] = header
+
     cv.imshow("Video", img)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
