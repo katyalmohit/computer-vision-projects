e925445fc4f33935c1e65d476de07abcc46ce1d7
katyalmohit <128162892+katyalmohit@users.noreply.github.com>
2024-07-31 23:13:11 +0530

    Define steps

--- hand-tracking/virtualMouseProject.py
+++ hand-tracking/virtualMouseProject.py
@@ -20,6 +20,25 @@ detector = htm.HandDetector(maxHands=1)
 while True:
     success, img = cap.read()
 
+    # 1. Find hand landmarks
+    # 2. Get the landmarks of tip of index and middle fingers
+    # 3. Check which fingers are up
+    # 4. Only index finger: Moving mode
+    # 5. Convert Coordinates
+    # 6. Smoothen Values
+    # 7. Move Mouse
+    # 8. Both Index and middle fingers are up: Clicking mode
+    # 9. Find distance between fingers
+    # 10. Click mouse if distance short
+
+    # 11. Frame Rate
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv.putText(img, str(f'FPS:{int(fps)}'), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+
+    # 12. Display
     cv.imshow("Video", img)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
